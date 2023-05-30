3ac23ddf679e51aaba4958b22832e82a6179a924
jorjvaan <jorjvaan@gmail.com>
2023-05-30 23:04:18 +0700

    Imported Unity Project

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int apples = 0;
+
+    [SerializeField] private AudioSource collectionSoundEffect;
+    [SerializeField] private Text applesText;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Apples"))
+        {
+            collectionSoundEffect.Play();
+            Destroy(collision.gameObject);
+            apples++;
+            applesText.text = "Apples: " + apples;
+        }
+    }
+}
